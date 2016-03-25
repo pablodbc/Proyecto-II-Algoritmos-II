67dcf89f5935c50ee6eb0c51317ed08fed6e03b1
Pablo <pablodbc30@gmail.com>
2016-03-24 23:02:20 -0430

    indice

--- proyecto.py
+++ proyecto.py
@@ -1,3 +1,4 @@
+from fnvhash import fnv1a_32 as fm 
 class Cancion:
 	def __init__(self,titulo,artista,genero,file=None):
 		self.titulo = titulo
@@ -50,6 +51,20 @@ class listaReproduccion:
 				node.prev = self.head.prev
 				self.head.prev = node
 				self.head = node
+				self.size+=1
+	def agregar_final(self,cancion):
+		node = nodoLista(cancion)
+		if self.head == None:
+			node.next = node
+			node.prev = node
+			self.head = node
+			self.size += 1
+		else:
+			if self.buscar(cancion) == None:
+				self.head.prev.next = node
+				node.prev = self.head.prev
+				self.head.prev = node
+				node.next = self.head
 				self.size+=1
 
 	def buscar(self,cancion):
@@ -135,11 +150,75 @@ class listaReproduccion:
 			print x.cancion.titulo,x.cancion.artista,x.cancion.genero
 			x = x.next
 
+class nodoIndice:
 
-t = listaReproduccion()
-t.agregar(Cancion("r","a","a"))
-t.mostrar()
-print
-t.ordenarArtista()
-t.mostrar()
+	def __init__(self,clave,lista):
+		self.clave = clave
+		self.lista = lista
+		self.next = None
+		self.prev = None
 
+class dlist:
+	def __init__(self):
+		self.head = None
+
+	def insert(self,node):
+		node.next = self.head
+		if self.head != None:
+			self.head.prev = node
+		self.head = node
+
+	def search(self, clave):
+		node = self.head
+		while node != None and node.clave != clave:
+			node = node.next
+		return node
+
+class Indice:
+	def __init__(self):
+		self.size = 5
+		self.slot = [dlist() for i in range(self.size)]
+		self.nodes = 0
+
+	def fh(self,clave):
+		return fm(clave)%self.size
+
+	def rehash(self):
+		N = self.size
+		A = list(self.slot)
+		self.size = 2*self.size + 1
+		self.slot = [ dlist() for i in xrange(self.size) ]
+		self.nodes = 0
+		for i in xrange(N):
+			x = A[i].head
+			while x!=None:
+				self.insertNode(x)
+				x = x.next
+
+	def insertNode(self, node):
+		m = self.fh(node.clave)
+		x = self.slot[m].search(node.clave)
+		if x == None:
+			self.slot[m].insert(node)
+			self.nodes += 1
+		else:
+			x.lista.agregar_final(node.lista.head)
+		if (float(self.nodes)/self.size)*100 >= 80:
+			self.rehash()
+
+	def insertArtista(self,cancion):
+		l = listaReproduccion()
+		l.agregar(cancion)
+		node = nodoIndice(cancion.artista,l)
+		self.insertNode(node)
+
+	def insertGenero(self,cancion):
+		l = listaReproduccion()
+		l.agregar(cancion)
+		node = nodoIndice(cancion.genero,l)
+		self.insertNode(node)
+
+	def search(self,clave):
+		m = self.fh(clave)
+		x = self.slot[m].search(clave)
+		if x != None : return x.lista
